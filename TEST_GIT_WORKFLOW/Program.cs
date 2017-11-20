a1168a5b80adcbd1c7c9da9eb663ac4b984ee101
alaedinee <alaedine.alami@gmail.com>
2017-11-20 23:36:54 +0100

    first commit

---
+++ TEST_GIT_WORKFLOW/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TEST_GIT_WORKFLOW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("test git flow");
+            Console.ReadKey();
+
+
+        }
+    }
+}
